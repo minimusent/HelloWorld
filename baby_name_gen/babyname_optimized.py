3b529e2021fcc0b719f348fc67b9443584fd4ef7
minimusent <34421556+minimusent@users.noreply.github.com>
2018-05-30 22:02:04 -0500

    Optimization
    Cut out 30 lines of code. Used a list instead of manual for loops for each letter.

--- baby_name_gen/babyname_optimized.py
+++ baby_name_gen/babyname_optimized.py
@@ -1,62 +1,27 @@
 import random, string
 
-letter_input_1 = input('Choose a letter..."v" for vowels, "c" for consonants, "l" for any other.')
-letter_input_2 = input('Choose a letter..."v" for vowels, "c" for consonants, "l" for any other.')
-letter_input_3 = input('Choose a letter..."v" for vowels, "c" for consonants, "l" for any other.')
-letter_input_4 = input('Choose a letter..."v" for vowels, "c" for consonants, "l" for any other.')
-letter_input_5 = input('Choose a letter..."v" for vowels, "c" for consonants, "l" for any other.')
+letter_input = []
+
+for i in range(0, 4):
+    letter_input.append(input('Choose a letter..."v" for vowels, "c" for consonants, "l" for any other: '))
+    print(i)
 
 vowels = 'aeiouy'
 consonants = 'bcdfghjklmnpqrstvwxz'
 letter = string.ascii_lowercase
 
 def generator():
-    if letter_input_1 == "v":
-        letter1 = random.choice(vowels)
-    elif letter_input_1 == "c":
-        letter1 = random.choice(consonants)
-    elif letter_input_1 == "v":
-        letter1 = random.choice(letter)
-    else:
-        letter1 = letter_input_1
+    for i in letter_input:
+        if letter_input == "v":
+            letter[i] = random.choice(vowels)
+        elif letter_input == "c":
+            letter[i] = random.choice(consonants)
+        elif letter_input == "v":
+            letter[i] = random.choice(letter)
+        else:
+            letter[i] = letter_input
+        name.append(letter[i])
 
-    if letter_input_2 == "v":
-        letter2 = random.choice(vowels)
-    elif letter_input_2 == "c":
-        letter2 = random.choice(consonants)
-    elif letter_input_2 == "v":
-        letter2 = random.choice(letter)
-    else:
-        letter2 = letter_input_2
-
-    if letter_input_3 == "v":
-        letter3 = random.choice(vowels)
-    elif letter_input_3 == "c":
-        letter3 = random.choice(consonants)
-    elif letter_input_3 == "v":
-        letter3 = random.choice(letter)
-    else:
-        letter3 = letter_input_3
-
-    if letter_input_4 == "v":
-        letter4 = random.choice(vowels)
-    elif letter_input_4 == "c":
-        letter4 = random.choice(consonants)
-    elif letter_input_4 == "v":
-        letter4 = random.choice(letter)
-    else:
-        letter4 = letter_input_4
-
-    if letter_input_5 == "v":
-        letter5 = random.choice(vowels)
-    elif letter_input_5 == "c":
-        letter5 = random.choice(consonants)
-    elif letter_input_5 == "v":
-        letter5 = random.choice(letter)
-    else:
-        letter5 = letter_input_5
-
-    name = letter1 + letter2 + letter3 + letter4 + letter5
     return (name)
 
 print(generator())
