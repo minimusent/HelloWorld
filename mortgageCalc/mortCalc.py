46e0a7bd62fba6addb69bf8d0cf17836dccab587
minimusent <34421556+minimusent@users.noreply.github.com>
2018-06-03 13:45:20 -0500

    Functions Added + Updates
    Separated action function

--- mortgageCalc/mortCalc.py
+++ mortgageCalc/mortCalc.py
@@ -11,18 +11,34 @@ def calcMonthlyPmt(salePrice, downPmt, intRate, term):
 def calcTotalPmt(monthlyPmt, term):
     return monthlyPmt * term
 
+def actionSelector(action, monthlyPmt, term):
+    action = action.lower()
+    if action == "m":
+        print("Total payment: $" + str(round(calcTotalPmt(monthlyPmt, term), 2)))
+    elif action == "t":
+        print("$" + str(round(monthlyPmt, 2)))
+    elif action == "i":
+        return
+
 
 def main():
     print("Welcome to the Mortgage Calculator! \n")
     salePrice = float(input("Sale Price: "))
     downPmt = int(input("Down payment: "))
     intRate = float(input("Annual interest rate: "))
+    term = 360
+    getAction(salePrice, downPmt, intRate, term)
+
+def getAction(salePrice, downPmt, intRate, term):
+    print("\nSale price: " + str(salePrice))
+    print("Down Payment: " +str(downPmt))
+    print("Interest Rate: " +str(intRate))
+    print("Terms in months: " +str(term))
     print("Available actions:\nM = monthly payment\nT = total payment")
     print("I = interest paid")
-    action = input("What would you like to calculate? \n")
-    term = 360
     monthlyPmt = calcMonthlyPmt(salePrice, downPmt, intRate, term)
-    print("$" + str(round(monthlyPmt, 2)))
-    print("Total payment: $" + str(round(calcTotalPmt(monthlyPmt, term), 2)))
+    action = input("What would you like to calculate? \n")
+    actionSelector(action, monthlyPmt, term)
+    getAction(salePrice, downPmt, intRate, term)
 
 main()
